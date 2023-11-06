74ac1a9262da04800d2429ebd05be1104af99322
Kishan Ved <124593234+Kishan-Ved@users.noreply.github.com>
2023-11-06 15:37:54 +0530

    Working fine

--- cube.c
+++ cube.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
-char C[3][8] = {
+char C[3][8] = { //coloring
     {'B','F','F','B','F','F','B','B'},
     {'D','L','D','R','U','R','U','L'},
     {'L','D','R','D','L','U','R','U'}
 };
-int c[8]={0,1,2,3,4,5,6,7};
-int x[8]={0,0,0,0,0,0,0,0};
+int c[8]={0,1,2,3,4,5,6,7}; //permutation - red values in pdf (ith location pe konsa cubie number)
+int x[8]={0,0,0,0,0,0,0,0}; //orientation - (follow red cubie initial locations in pdf)
 int temp[8];
-int Moves[3][8]={
+int Moves[3][8]={ //How permutations change
     {0,4,1,3,5,2,6,7}, //F
     {0,1,5,2,4,6,3,7}, //R
     {0,1,2,3,7,4,5,6}  //U
 };
-int Offsets[3][8]={
+int Offsets[3][8]={ //How orientations change (+ kitna)
     {0,0,0,0,0,0,0,0}, //F
     {0,0,2,1,0,1,2,0}, //R
     {0,0,0,0,1,2,1,2}  //U
 };
 char MoveNames[]={'F','R','U'};
-int Faces[3][2][2]={
+int Faces[3][2][2]={ //These are locations (initial red cubie locations in pdf)
 { //Front
     {4,5},
     {1,2}
@@ -39,7 +39,7 @@ void Disp(){
         printf("%s : \n",FacesNames[k]);
         for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
-                int loc = Faces[k][i][j];
+                int loc = Faces[k][i][j]; //Ye location hai
                 //printf("loc : %d\n",loc);
                 //printf("offset : %d\n",offset);
                 //printf("x[loc] :%d\n",x[loc]);
@@ -84,10 +84,10 @@ void applymove(int i){
 long int facts[8];
 long int toN(){
     int N = 0;
-    for(int i=2;i<8;i++){
+    for(int i=2;i<8;i++){ //indexing orientations
         N = 3*N+x[i];
     }
-    N = N*facts[7];
+    N = N*facts[7]; //facts[i] stores i factorial
     for(int i=0;i<8;i++){
         temp[i]=c[i];
     }
@@ -95,21 +95,22 @@ long int toN(){
         for(int j=1;j<=i;j++){
             if(temp[j]>=temp[i]){temp[j]=temp[j]-1;}
         }
-        N += (temp[i])*(facts[i-1]);
+        N += (temp[i])*(facts[i-1]); //final index
     }
     return N;
 }
 long int Nx;
 void toC(int N){
-    Nx = N/facts[7];
-    N = N%facts[7];
+    Nx = N/facts[7]; //orientation
+    N = N%facts[7]; //permutation
     int s = 0;
     for(int i=7;i>1;i--){
         x[i]=Nx%3;
         Nx=Nx/3;
         s=(s+x[i])%3;
     }
-    x[1]=3-s;
+    // x[0] is always 0
+    x[1]=3-s; //sum = 0
     for(int i=7;i>0;i--){
         c[i]=N/facts[i-1];
         N=N%facts[i-1];
@@ -121,7 +122,7 @@ void toC(int N){
         c[i]=c[i]+1;
     }
 }
-int legal(){
+int legal(){ //sum = 0 in orientations or permutations have repeated numbers
     for(int i=0;i<8;i++){
         temp[i]=0;
     }
@@ -140,17 +141,25 @@ int legal(){
         }
     return (s==0);
 }
-int n0 = toN();
+
 int visited[3674160];
-int Q[3674160];
+int Q[3674160]; //This is a queue
 long int S[3674160];
 long int cur = 0;
 long int last = 0;
 long int N_maxn1=3674160;
-long int n=toN();
+
 //initial state
-int xn[8]={0,1,2,1,2,1,2,1};
-int cn[8]={0,1,3,4,2,6,7,5};
+// Testcase: User moves R'
+int xn[8]={0,0,2,1,0,1,2,0}; //user input of orientations (of front and back face, order is red numbers in pdf denoting initial state)
+int cn[8]={0,1,3,6,4,2,5,7}; //user input of permutations
+
+// Testcase: User 
+// int xn[8] = {0,0,0,0,0,0,0,0};
+// int cn[8] = {0,1,2,3,4,5,6,7};
+
+// Testcase 2 in main() function
+
 int sol[100];
 long int s;
 void printSol(int s){
@@ -160,6 +169,7 @@ void printSol(int s){
         i++;
         s=s/4;
     }
+    i--; // As we do i++ in the end
     for(int j=i;j>=0;j--){
         printf("%c",MoveNames[sol[j]]);
     }
@@ -167,40 +177,50 @@ void printSol(int s){
 }
 int done = 0;
 int main(){
-    for(int i=0;i<8;i++){
-        x[i]=xn[i];
-        c[i]=cn[i];
-    }
-    printf("Initial state :\n\n");
-    Disp();
-    if(!legal()){
-        printf("This isn't a legal state. That is to say; your cube cannot be solved.\n");
-        return 0;
-    }
     facts[0]=1;
     for(int i=1;i<8;i++){
         facts[i]=facts[i-1]*i;
     }
+
+    long int n0 = toN(); //initial state's index, this is always 0
+    long int n;
+
+    printf("%ld\n",Q[0]);
+    for(int i=0;i<8;i++){
+        x[i]=xn[i];
+        c[i]=cn[i];
+    }
+    // Testcase 2: where user does front move after the above moves
+    // applymove(0);
+    printf("Initial state :\n\n");
+    Disp();
+
+    if(!legal()){
+        printf("This isn't a legal state. That is to say; your cube cannot be solved.\n");
+        return 0;
+    }
+
     for(int i=0;i<N_maxn1;i++){
         visited[i]=0;
     }
+
     visited[toN()]=1;
-    Q[0]=toN();
+    Q[0]=toN(); //This is a queue
     S[0]=0;
     for(int iterat=0;iterat<N_maxn1;iterat++){
         if(done){break;}
-        n = Q[cur];
-        s = S[cur];
-        toC(n);
-        for(int move=0;move<3;move++){
-            applymove(move);
-            n = toN();
-            if(n==n0){
+        n = Q[cur]; //current index
+        s = S[cur]; //current solution (ab tak ka) as a number
+        toC(n); //decoding the index, this changes x and c
+        if(n==n0){
                 printf("found solution : ");
                 printSol(s);
                 done = 1;
                 break;
-            }
+        }
+        for(int move=0;move<3;move++){
+            applymove(move);
+            n = toN();
             if(visited[n]==0){
                 last++;
                 Q[last]=n;
